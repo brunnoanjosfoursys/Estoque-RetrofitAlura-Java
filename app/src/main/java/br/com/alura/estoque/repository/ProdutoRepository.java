22a6a11192a0dbdb86c2d48e6c393a1d72d5b46f
brunnobird <bird.tec.gonzalez@gmail.com>
2022-04-25 09:53:01 -0300

    Criando um repository

---
+++ app/src/main/java/br/com/alura/estoque/repository/ProdutoRepository.java
@@ -0,0 +1,58 @@
+package br.com.alura.estoque.repository;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.util.List;
+
+import br.com.alura.estoque.asynctask.BaseAsyncTask;
+import br.com.alura.estoque.database.dao.ProdutoDAO;
+import br.com.alura.estoque.model.Produto;
+import br.com.alura.estoque.retrofit.EstoqueRetrofit;
+import br.com.alura.estoque.retrofit.service.ProdutoService;
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class ProdutoRepository {
+    private final ProdutoDAO dao;
+
+    public ProdutoRepository(ProdutoDAO dao) {
+        this.dao = dao;
+    }
+
+    public void buscaProdutos(ProdutosCarregadosListener listener) {
+        //Buscando Dados locais com ROOM, enquanto a outra asynctask da requisição ainda não terminou
+        buscaProdutosInternos(listener);
+    }
+
+    private void buscaProdutosInternos(ProdutosCarregadosListener listener) {
+        new BaseAsyncTask<>(dao::buscaTodos,
+                resultado -> {
+                    // Notifica que o dado esta pronto
+                    listener.quandoCarregados(resultado);
+                    buscaProdutosNaApi(listener);
+                }).execute();
+    }
+
+    private void buscaProdutosNaApi(ProdutosCarregadosListener listener) {
+        ProdutoService service = new EstoqueRetrofit().getProdutoService();
+        Call<List<Produto>> call = service.buscaTodos();
+
+        new BaseAsyncTask<>(() -> {
+            //Execucao sincrona
+            try {
+                Response<List<Produto>> resposta = call.execute();
+                List<Produto> produtosNovos = resposta.body();
+                dao.salva(produtosNovos);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return dao.buscaTodos();
+        },//Notifica que o dado está pronto
+                listener::quandoCarregados).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+    public interface ProdutosCarregadosListener {
+        void quandoCarregados(List<Produto> produtos);
+    }
+}
--- app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
+++ app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
@@ -1,30 +1,21 @@
 package br.com.alura.estoque.ui.activity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.IOException;
-import java.util.List;
-
 import br.com.alura.estoque.R;
 import br.com.alura.estoque.asynctask.BaseAsyncTask;
 import br.com.alura.estoque.database.EstoqueDatabase;
 import br.com.alura.estoque.database.dao.ProdutoDAO;
 import br.com.alura.estoque.model.Produto;
-import br.com.alura.estoque.retrofit.EstoqueRetrofit;
-import br.com.alura.estoque.retrofit.service.ProdutoService;
+import br.com.alura.estoque.repository.ProdutoRepository;
 import br.com.alura.estoque.ui.dialog.EditaProdutoDialog;
 import br.com.alura.estoque.ui.dialog.SalvaProdutoDialog;
 import br.com.alura.estoque.ui.recyclerview.adapter.ListaProdutosAdapter;
-import retrofit2.Call;
-import retrofit2.Response;
 
 public class ListaProdutosActivity extends AppCompatActivity {
 
@@ -44,39 +35,9 @@ public class ListaProdutosActivity extends AppCompatActivity {
         EstoqueDatabase db = EstoqueDatabase.getInstance(this);
         dao = db.getProdutoDAO();
 
-        buscaProdutos();
-    }
+        ProdutoRepository repository = new ProdutoRepository(dao);
+        repository.buscaProdutos(adapter::atualiza);
 
-    private void buscaProdutos() {
-        ProdutoService service = new EstoqueRetrofit().getProdutoService();
-        Call<List<Produto>> call = service.buscaTodos();
-
-        //Buscando Dados locais com ROOM, enquanto a outra asynctask da requisição ainda não terminou
-        new BaseAsyncTask<>(dao::buscaTodos,
-                resultado -> {
-                    adapter.atualiza(resultado);
-                    new BaseAsyncTask<>(() -> {
-                        //Execucao sincrona
-                        try {
-                            Thread.sleep(3000);
-                            Response<List<Produto>> resposta = call.execute();
-                            List<Produto> produtosNovos = resposta.body();
-                            dao.salva(produtosNovos);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        return dao.buscaTodos();
-                    }, produtosNovos -> {
-                        //Quando recebermos os produtos, oque queremos fazer?
-                        if (produtosNovos != null) {
-                            adapter.atualiza(produtosNovos);
-                        } else {
-                            Toast.makeText(this, "Não foi possível buscar os produtos da API", Toast.LENGTH_SHORT).show();
-                        }
-                    }).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-                }).execute();
     }
 
     private void configuraListaProdutos() {
