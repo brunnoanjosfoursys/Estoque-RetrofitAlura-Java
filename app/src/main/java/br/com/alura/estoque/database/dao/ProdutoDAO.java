53590c30b6e32a39ba275271018414d4448d3916
brunnobird <bird.tec.gonzalez@gmail.com>
2022-04-25 12:11:25 -0300

    Refatorando o callback de comunicacao retrofit do POST

--- app/src/main/java/br/com/alura/estoque/database/dao/ProdutoDAO.java
+++ app/src/main/java/br/com/alura/estoque/database/dao/ProdutoDAO.java
@@ -8,6 +8,7 @@ import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
+
 import br.com.alura.estoque.model.Produto;
 
 @Dao
--- app/src/main/java/br/com/alura/estoque/repository/ProdutoRepository.java
+++ app/src/main/java/br/com/alura/estoque/repository/ProdutoRepository.java
@@ -11,21 +11,25 @@ import br.com.alura.estoque.model.Produto;
 import br.com.alura.estoque.retrofit.EstoqueRetrofit;
 import br.com.alura.estoque.retrofit.service.ProdutoService;
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.internal.EverythingIsNonNull;
 
 public class ProdutoRepository {
     private final ProdutoDAO dao;
+    private final ProdutoService service;
 
     public ProdutoRepository(ProdutoDAO dao) {
         this.dao = dao;
+        service = new EstoqueRetrofit().getProdutoService();
     }
 
-    public void buscaProdutos(ProdutosCarregadosListener listener) {
+    public void buscaProdutos(DadosCarregadosListener<List<Produto>> listener) {
         //Buscando Dados locais com ROOM, enquanto a outra asynctask da requisição ainda não terminou
         buscaProdutosInternos(listener);
     }
 
-    private void buscaProdutosInternos(ProdutosCarregadosListener listener) {
+    private void buscaProdutosInternos(DadosCarregadosListener<List<Produto>> listener) {
         new BaseAsyncTask<>(dao::buscaTodos,
                 resultado -> {
                     // Notifica que o dado esta pronto
@@ -34,7 +38,7 @@ public class ProdutoRepository {
                 }).execute();
     }
 
-    private void buscaProdutosNaApi(ProdutosCarregadosListener listener) {
+    private void buscaProdutosNaApi(DadosCarregadosListener<List<Produto>> listener) {
         ProdutoService service = new EstoqueRetrofit().getProdutoService();
         Call<List<Produto>> call = service.buscaTodos();
 
@@ -52,7 +56,55 @@ public class ProdutoRepository {
                 listener::quandoCarregados).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
-    public interface ProdutosCarregadosListener {
-        void quandoCarregados(List<Produto> produtos);
+    public void salva(Produto produto, DadosCarregadosCallback<Produto> callback) {
+        salvaNaApi(produto, callback);
+    }
+
+    private void salvaNaApi(Produto produto, DadosCarregadosCallback<Produto> callback) {
+        Call<Produto> call = service.salva(produto);
+
+        //enqueue = método para executar asincrona sem ter a necessidade de fazer uma asyncTask, não precisamos usar            o execute tambem usando ele
+        call.enqueue(new Callback<Produto>() {
+            @Override
+            @EverythingIsNonNull
+            //Marcando com essa anotação pois o retrofit garante que o valor não vai ser null
+            public void onResponse(Call<Produto> call, Response<Produto> response) {
+                //Verificando se a resposta foi sucesso
+                if (response.isSuccessful()) {
+                    Produto produtoSalvo = response.body();
+                    if (produtoSalvo != null) {
+                        salvaInterno(produtoSalvo, callback);
+                    }
+                } else {
+                    //Notificar falha
+                    callback.quandoFalha("Resposta não sucedida");
+                }
+            }
+
+            @Override
+            @EverythingIsNonNull
+            public void onFailure(Call<Produto> call, Throwable t) {
+                //Notificar falha
+                callback.quandoFalha("Falha de comunicação: " + t.getMessage() );
+            }
+        });
+    }
+
+    private void salvaInterno(Produto produto, DadosCarregadosCallback<Produto> callback) {
+        new BaseAsyncTask<>(() -> {
+            long id = dao.salva(produto);
+            return dao.buscaProduto(id);
+            //Notifica a nossa activity
+        }, callback::quandoSucesso)
+                .execute();
+    }
+
+    public interface DadosCarregadosListener<T> {
+        void quandoCarregados(T resultado);
+    }
+
+    public interface DadosCarregadosCallback <T> {
+        void quandoSucesso(T resultado);
+        void quandoFalha(String erro);
     }
 }
